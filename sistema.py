27df768dcebbf4be82144ab6d1f9c4c1d590f3b5
aljuliedot <julietagonzalezaldana91@gmail.com>
2026-06-20 22:00:23 -0300

    Crear sistema.py
    Se creo el archivo principal.

---
+++ sistema.py
@@ -0,0 +1,27 @@
+class biblioteca():
+    def __init__(self):
+        pass
+
+class usuario():
+    def __init__(self):
+        pass
+
+class libro():
+    def __init__(self):
+        pass
+        
+class prestamo():
+    def __init__(self):
+        pass
+        
+class estrategia_recomendacion():
+    def __init__(self):
+        pass
+       
+class historial():
+    def __init__(self):
+        pass
+        
+class bibliotecario():
+    def __init__(self):
+        pass
